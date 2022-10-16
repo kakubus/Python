beb1e2cc9f5ef95f72af7cf35018f3697946009f
Kuba <kakubus@outlook.com>
2022-10-16 17:01:53 +0200

    Test

---
+++ test.py
@@ -0,0 +1,16 @@
+i = -20
+while i<0:
+  i+=1
+  print(i)
+print("Koniec :D")
+
+for i in range(10):
+    print(i)
+    
+a = [3, 5, 7, 9, 10, 20]
+for i in a: #pooglądać pętle w pajtonQ
+    print(a[i-1])
+
+
+
+  
